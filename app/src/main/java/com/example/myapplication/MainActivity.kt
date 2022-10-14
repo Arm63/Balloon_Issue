91f2472ab08571c7f7638903e12d89d85eb59505
Armen Asatryan <armen.asatryan@inconceptlabs.com>
2022-10-15 00:06:07 +0400

    Show pop up under pop up

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,30 +2,33 @@ package com.example.myapplication
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.PopupBoardMoreMenuBinding
-import com.skydoves.balloon.*
+import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bindingPopUp: PopupBoardMoreMenuBinding
+    private lateinit var bindingPopUpUnderBalloon: PopupBoardMoreMenuBinding
 
-    private val balloon: Balloon by lazy{
+    private lateinit var bindingPopUpUnderPopUp: PopupBoardMoreMenuBinding
+    private lateinit var bindingPopUpPopUpAbove: PopupBoardMoreMenuBinding
+
+    private val balloon: Balloon by lazy {
         Balloon.Builder(this)
             .setIsVisibleOverlay(true) // comment this line and project will work but without overlay
             .setOverlayColorResource(R.color.purple_200)
             .setArrowOrientation(ArrowOrientation.BOTTOM)
-            .setBalloonAnimation(BalloonAnimation.OVERSHOOT)
-            .setDismissWhenTouchOutside(false)
-            .setDismissWhenShowAgain(false)
-            .setDismissWhenOverlayClicked(false)
-            .setLifecycleOwner(this)
-            .build()
+            .setBalloonAnimation(BalloonAnimation.OVERSHOOT).setDismissWhenTouchOutside(false)
+            .setDismissWhenShowAgain(false).setDismissWhenOverlayClicked(false)
+            .setLifecycleOwner(this).build()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,27 +37,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bindingPopUp = PopupBoardMoreMenuBinding.inflate(layoutInflater)
+        bindingPopUpUnderBalloon = PopupBoardMoreMenuBinding.inflate(layoutInflater)
 
+        bindingPopUpUnderPopUp = PopupBoardMoreMenuBinding.inflate(layoutInflater)
+        bindingPopUpPopUpAbove = PopupBoardMoreMenuBinding.inflate(layoutInflater)
     }
 
     override fun onResume() {
         super.onResume()
 
-        val popupWindow = PopupWindow(bindingPopUp.root, WRAP_CONTENT, WRAP_CONTENT).apply {
+        setPopUpFunctionality()
+    }
+
+    private fun setPopUpFunctionality(){
+        val popupWindow = PopupWindow(bindingPopUpUnderPopUp.root, WRAP_CONTENT, WRAP_CONTENT).apply {
             isFocusable = true
             isOutsideTouchable = true
             overlapAnchor = true
         }
 
-        binding.anchorView.setOnClickListener {
-            popupWindow.showAsDropDown(binding.anchorView, 0, 0)
+        val popupWindowAboveWindow = PopupWindow(bindingPopUpPopUpAbove.root, WRAP_CONTENT, WRAP_CONTENT).apply {
+            isFocusable = true
+            isOutsideTouchable = true
+            overlapAnchor = true
+        }
+
+        binding.anchorForPopup.setOnClickListener {
+            popupWindow.showAsDropDown(binding.anchorForPopup, 0, 0)
             popupWindow.contentView.setBackgroundColor(Color.GRAY)
 
-            val childViewFromPopupWindow = popupWindow.contentView.findViewById<TextView>(R.id.second_text)
-            childViewFromPopupWindow.post {
-                balloon.showAlignBottom(popupWindow.contentView)
-            }
+            val childViewFromPopupWindow =
+                popupWindow.contentView.findViewById<TextView>(R.id.second_text)
+
+            popupWindowAboveWindow.showAsDropDown(childViewFromPopupWindow, 80, 80)
+            popupWindowAboveWindow.contentView.setBackgroundColor(Color.GRAY)
         }
     }
 }
